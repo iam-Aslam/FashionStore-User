cb3852ff54a8d6af8147c3f8bfd8c9b921c586a7
Mohammed Aslam M K <117348902+iam-Aslam@users.noreply.github.com>
2023-06-05 00:34:47 +0530

    home page designing image added

--- lib/presentations/home/widget/home_widget.dart
+++ lib/presentations/home/widget/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:fashionstore/core/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'category_button_widget.dart';
@@ -16,64 +17,94 @@ class WidgetHome extends StatelessWidget {
         child: Scaffold(
             body: Padding(
       padding: const EdgeInsets.only(left: 15, right: 15, top: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const HeadWidget(),
-          khieght20,
-          Text(
-            'Welcome,',
-            style: GoogleFonts.prompt(
-              textStyle: const TextStyle(
-                  letterSpacing: .5,
-                  fontSize: 30,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const HeadWidget(),
+            khieght20,
+            Text(
+              'Welcome,',
+              style: GoogleFonts.prompt(
+                textStyle: const TextStyle(
+                    letterSpacing: .5,
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          Text(
-            'Our Fashion Store',
-            style: GoogleFonts.prompt(
-              textStyle: const TextStyle(
-                  letterSpacing: .5,
-                  fontSize: 25,
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w600),
+            Text(
+              'Our Fashion Store',
+              style: GoogleFonts.prompt(
+                textStyle: const TextStyle(
+                    letterSpacing: .5,
+                    fontSize: 25,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w600),
+              ),
             ),
-          ),
-          khieght20,
-          const SearchWidget(),
-          khieght30,
-          const ExclusiveProductWidget(),
-          khieght20,
-          sideHeading(text: 'Categories'),
-          khieght10,
-          const Row(
-            children: [
-              CategoryButton(name: 'Dresses', active: true),
-              CategoryButton(name: 'Jackets', active: false),
-              CategoryButton(name: 'Jeans', active: false),
-              CategoryButton(name: 'Shoese', active: false),
-            ],
-          ),
-          sideHeading(text: 'Top Dresses'),
-          khieght10,
-          Expanded(
-            child: GridView.builder(
+            khieght20,
+            const SearchWidget(),
+            khieght30,
+            const ExclusiveProductWidget(),
+            khieght20,
+            sideHeading(text: 'Categories'),
+            khieght10,
+            const Row(
+              children: [
+                CategoryButton(name: 'Dresses', active: true),
+                CategoryButton(name: 'Jackets', active: false),
+                CategoryButton(name: 'Jeans', active: false),
+                CategoryButton(name: 'Shoese', active: false),
+              ],
+            ),
+            Row(
+              children: [
+                sideHeading(text: 'Top Dresses'),
+                const Spacer(),
+                TextButton(onPressed: () {}, child: const Text('View All'))
+              ],
+            ),
+            khieght5,
+            GridView.builder(
+              shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
               itemCount: 10,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
-                  crossAxisSpacing: 4.0,
-                  mainAxisSpacing: 4.0),
+                  crossAxisSpacing: 16.0,
+                  mainAxisSpacing: 16.0),
               itemBuilder: (context, index) {
                 return Container(
-                  color: Colors.yellow,
+                  //color: Colors.yellow,
+                  width: 80,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/nike.jpeg'),
+                        fit: BoxFit.cover,
+                      )),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          left: 125,
+                          top: 0,
+                          child: IconButton(
+                            icon: const Icon(
+                              CupertinoIcons.heart_circle,
+                              color: Colors.white,
+                              size: 24,
+                            ),
+                            onPressed: () {},
+                          ))
+                    ],
+                  ),
                 );
               },
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     )));
   }
